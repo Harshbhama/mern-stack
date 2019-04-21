b0e0d3cc199f220396a26e614d58c9795159355c
harsh bhama <harshbhama96@gmail.com>
2019-04-21 16:35:20 +0530

    setup node and mongodb

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
+import TodoList from "./components/todos-list.component"
+import EditTodo from "./components/edit-todo.component"
+import CreateTodo from "./components/create-todo.component"
+
+import { Link } from "react-router-dom"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className = "container">
+          <h1>Hello</h1>
+          <ul>
+            <li><Link to = '/' > Todolist</Link></li>
+            <li><Link to = '/edit/:id' > EditTodo</Link></li>
+            <li><Link to = '/create' > CreateTodo</Link></li>
+          </ul>
+          <Switch>
+            <Route exact path = '/' component = {TodoList} />
+            <Route exact path = '/edit/:id' component = {EditTodo} />
+            <Route exact path = '/create' component = {CreateTodo} />
+          </Switch>
+        </div>
+          
+        
     );
   }
 }
